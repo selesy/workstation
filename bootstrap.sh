fb53c4b93745df98e0df042f10276db0bd566920
Steve Moyer <smoyer@psu.edu>
2014-12-02 13:25:17 -0500

    The bootstrap.sh and bootstrap.yml files set up a remote host for control by ansible (on this machine).

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get -y install openssh-server
+
+if [ $# -gt 0 ] && [ -n $1 ] ; then
+  echo "Target host: $1"
+else
+  echo "This script requires one argument that contains an IP address"
+  echo "or hostname."
+fi
+
+echo "[targets]" > ansible_inventory.ini
+echo "" >> ansible_inventory.ini
+echo "$1" >> ansible_inventory.ini
+
+ansible-playbook -K -i ansible_inventory.ini bootstrap.yml
+
