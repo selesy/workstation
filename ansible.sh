1b68a9fcd91a770affe56002fe10d8041761fb14
Steve Moyer <smoyer@psu.edu>
2016-05-19 14:50:18 -0400

    Used the version of Ansible (now > 2.0) packaged with Ubuntu.

--- ansible.sh
+++ ansible.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install git
-
-sudo apt-get -y install software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-
-sudo apt-get -y install ansible
+sudo apt-get -y install git ansible
 
 if [ ! -d ~/.ssh ] ; then
   mkdir ~/.ssh
