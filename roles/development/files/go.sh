5acd11a75afd64698b9534aba84ce205b3860fbc
Steve Moyer <smoyer@psu.edu>
2019-01-16 10:12:07 -0500

    Fixes problem installing Go tools
    Resolves #132

--- roles/development/files/go.sh
+++ roles/development/files/go.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export GOROOT=/opt/go/{{ go_version }}
-export GOPATH=~/go
-export PATH=$PATH:$GOROOT/bin
+export GOPATH=~/go:{{ go_tools_directory }}
+export PATH=$PATH:$GOROOT/bin:{{ go_tools_directory }}/bin
