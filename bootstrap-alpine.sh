26f7dfa492d81912ca794e098c2354c83ce2e85d
Steve Moyer <smoyer@psu.edu>
2019-07-19 15:50:30 -0400

    Consolidates bootstrap playbooks
    References #143

--- bootstrap-alpine.sh
+++ bootstrap-alpine.sh
@@ -17,5 +17,5 @@ echo "" >> ansible_inventory.ini
 echo "$1" >> ansible_inventory.ini
 
 export ANSIBLE_NOCOWS=1
-ansible-playbook -k -K -i ansible_inventory.ini bootstrap-alpine.yml
+ansible-playbook -k -K -i ansible_inventory.ini bootstrap.yml
 
