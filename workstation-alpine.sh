cb65ecff9e8d840bd470371515a06d8e26a696c0
Steve Moyer <smoyer@psu.edu>
2019-07-23 15:47:26 -0400

    Unifies Ubuntu and Alpine scripts

--- workstation-alpine.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export ANSIBLE_NOCOWS=1
-ansible-playbook --private-key=~/.ssh/ansible-key -i ansible_inventory.ini workstation.yml $*
-
--- workstation.sh
+++ workstation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 export ANSIBLE_NOCOWS=1
 ansible-playbook --private-key=~/.ssh/ansible-key -i ansible_inventory.ini workstation.yml $*
