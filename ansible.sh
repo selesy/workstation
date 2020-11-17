88711325a0e537b4b2877e231d3bef65c5a5e790
Steve Moyer <smoyer1@selesy.com>
2020-11-17 09:49:26 -0500

    Adds signing key for Ansible repository

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get install software-properties-common
-sudo apt-add-repository --yes 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367
+sudo apt-add-repository --yes 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main'
 sudo apt-get update
 sudo apt-get -y install git ansible
 sudo apt-get -y install openssh-client
