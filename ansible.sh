6ea276c94f34dc2ceeac52dc51a19986e9d8ad87
Steve Moyer <smoyer1@selesy.com>
2022-05-14 07:55:08 -0400

    feat: install ansible-lint
    Resolves #171

--- ansible.sh
+++ ansible.sh
@@ -4,7 +4,7 @@ sudo apt-get install software-properties-common
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367
 sudo apt-add-repository --yes 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main'
 sudo apt-get update
-sudo apt-get -y install git ansible
+sudo apt-get -y install git ansible ansible-lint
 sudo apt-get -y install openssh-client
 sudo apt-get -y install openssh-server
 
