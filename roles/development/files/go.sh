a1d46ff8a81b232c8cc81ea402c3583bd1858696
Steve Moyer <smoyer1@selesy.com>
2022-09-10 12:18:53 -0400

    feat(asdf): install Go via asdf
    References #350.

--- roles/development/files/go.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# export GOROOT=/opt/go/{{ go_version }}
-export GOPATH=~/go:{{ go_tools_directory }}
-# export PATH=$PATH:$GOROOT/bin:{{ go_tools_directory }}/bin:~/go/bin
-export PATH={{ go_tools_directory }}/bin:$PATH
--- roles/development/files/go_completion.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-complete -C /usr/share/go-tools/bin/gocomplete go
