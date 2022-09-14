b2da4fe74d9aaf4d7f373361406cc6758b799083
Steve Moyer <smoyer1@selesy.com>
2022-09-14 10:36:39 -0400

    fix(350): overcome Github rate limiting
    References #350.

--- workstation.sh
+++ workstation.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export ANSIBLE_NOCOWS=1
-ansible-playbook -K -i localhost, workstation.yml $*
+ansible-playbook -K -i localhost, -e GITHUB_API_TOKEN=$GITHUB_API_TOKEN workstation.yml $*
