031ec92268dba36d4980451a869c081a879737ca
Steve Moyer <smoyer1@selesy.com>
2024-07-02 11:28:54 -0400

    feat(bootstrap): change for Debian Bookworm and (mostly) userspace

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ $# -gt 0 ] && [ -n $1 ] ; then
-  echo "Target host: $1"
-else
-  echo "This script requires one argument that contains an IP address"
-  echo "or hostname."
-  exit 1
-fi
+#
+# Install ASDF
+#
+sudo apt install git curl
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 
-ssh-keyscan -H $1 >> ~/.ssh/known_hosts
+#
+# Install Ansible
+#
+~/.asdf/bin/asdf plugin add ansible-base
+~/.asdf/bin/asdf install ansible-base latest
+~/.asdf/bin/asdf global ansible-base latest
 
-sudo apt-get -y install openssh-server sshpass
-
-echo "[targets]" > ansible_inventory.ini
-echo "" >> ansible_inventory.ini
-echo "$1" >> ansible_inventory.ini
-
-export ANSIBLE_NOCOWS=1
-ansible-playbook -k -K -i ansible_inventory.ini bootstrap.yml
-
+#
+# Install Workstation
+#
+git clone https://github.com/selesy/workstation ~/.workstation --branch bookworm
