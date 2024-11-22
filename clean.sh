64e55598b0fc75885b32c08ac1e107f9d3f6fec9
Steve Moyer <smoyer1@selesy.com>
2024-11-22 16:20:59 -0500

    refactor(setup): split all roles into main (install) and clean files

--- clean.sh
+++ clean.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 export ANSIBLE_NOCOWS=1
-ansible-playbook -K -i localhost, clean.yml $*
-
-rm -f ansible_inventory.ini
+ansible-playbook -K -i localhost, -e GITHUB_API_TOKEN=$GITHUB_API_TOKEN clean.yaml $*
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export ANSIBLE_NOCOWS=1
+ansible-playbook -K -i localhost, -e GITHUB_API_TOKEN=$GITHUB_API_TOKEN install.yaml $*
